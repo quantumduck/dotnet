f5a4353825b5a63ff4b22a3cfc73683a86385bf7
quantumduck <quantum.duck@hotmail.com>
2017-06-20 16:13:52 -0400

    silly example

---
+++ exp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace exp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          if (args.Length == 0)
+          {
+            Console.WriteLine("You gotta give me something!");
+          }
+          else
+          {
+            Console.WriteLine($"I'm sorry, I don't know what {args[0]} is.");
+          }
+        }
+    }
+}
